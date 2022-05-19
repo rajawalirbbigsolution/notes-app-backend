19744d55140853bdc786ddc1e72bacadcffd2c00
big-web-19 <bansos.dnr@gmail.com>
2022-05-19 15:37:24 +0700

    first commit

---
+++ src/notes.js
@@ -0,0 +1,13 @@
+const notes = [
+    {
+        id: string,
+        title: string,
+        createdAt: string,
+        updatedAt: string,
+        tags: array,
+        body: string,
+       }
+];
+
+ 
+module.exports = notes;
